069bdf4e695ce655bcb5b41fe3ea4578866227e2
Jacob MacFarlane <jmacfarlane22@turing.edu>
2023-06-19 14:29:44 -0700

    shows a;; the work been done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './App.css';
+import{ NavBar } from './components/NavBar/NavBar';
+import { Banner } from './components/Banner/Banner';
+import { Skills } from './components/Skills/Skills';
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Banner />
+      <Skills />
+    </div>
+  );
+}
+
+export default App;
